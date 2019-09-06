4e5f7205cca2ac9f172c8f8b8a73c39e0305b308
Lidcer <47243657+Lidcer@users.noreply.github.com>
2019-09-07 00:01:52 +0200

    Update index.js

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client(["0000000000000000"]); // put admins id in to 
 const BOT_TOKEN = process.env.BOT_TOKEN; // Your bot token
 client.clearSettingsTimeOut = 1000; //Admins settings are going to get cleared overtime use this to set custom clear time (default) minute
 client.warnMessage = true; //This is going to tell user who is not admin that they are not admin (disabled on default)
-client.prefix = "sudo"; //You can set custom admin prefix with this default (sudo)
+client.prefix = "sudo"; //You can set custom admin prefix with this (default is sudo)
 
 //Other things are the same as discord js (discord.js.org)
 client.on("ready", () => {
